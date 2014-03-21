99dc13b7dde17fddfdf84a985771db404301e925
Daniel Bordak <dbordak@gmail.com>
2014-03-20 20:31:02 -0400

    Configured whitespace

--- config/init-whitespace.el
+++ config/init-whitespace.el
@@ -1,3 +1,7 @@
-(whitespace-mode 1)
+(global-whitespace-mode 1)
+(diminish 'whitespace-mode)
+(diminish 'global-whitespace-mode)
+
+(setq whitespace-style '(trailing tabs tab-mark))
 
 (provide 'init-whitespace)
