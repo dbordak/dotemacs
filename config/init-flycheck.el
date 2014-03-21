5e79d236c7c7c299a2d573a9f60400f522f52a61
Daniel Bordak <dbordak@gmail.com>
2014-03-21 19:37:49 -0400

    Moved Flycheck to right fringe

--- config/init-flycheck.el
+++ config/init-flycheck.el
@@ -4,6 +4,7 @@
   (setq flycheck-check-syntax-automatically '(save mode-enabled))
   (setq flycheck-checkers (delq 'emacs-lisp-checkdoc flycheck-checkers))
   (setq flycheck-checkers (delq 'html-tidy flycheck-checkers)))
+  (setq flycheck-indication-mode 'right-fringe)
 
 (global-flycheck-mode t)
 
