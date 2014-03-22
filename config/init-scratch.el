c8208adb82acaa54c79fd86e5167f4b9c82b67e2
Daniel Bordak <dbordak@gmail.com>
2014-03-22 06:22:43 -0400

    Actually added it this time.

---
+++ config/init-scratch.el
@@ -0,0 +1,9 @@
+;; I'm not very Lispy.
+(setq initial-major-mode 'text-mode)
+
+;; Should hijack scratch buffer, doesn't seem to.
+(setq remember-notes-buffer-name "*scratch*")
+
+(setq initial-buffer-choice 'remember-notes)
+
+(provide 'init-scratch)
